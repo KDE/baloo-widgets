80c5d1f55ee4bb3352118d194d0247024701c921
Pino Toscano <pino@kde.org>
2012-08-31 12:49:06 +0200

    Messages.sh: comment out tips extraction, remove generated files

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,6 @@
 #!bin/sh
  
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$PREPARETIPS > tips.cpp
+#$PREPARETIPS > tips.cpp
 $XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/nepomukwidgets.pot
+rm -f rc.cpp tips.cpp
