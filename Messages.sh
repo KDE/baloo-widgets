a2db847116033524dbaf0d3c57e23bac6995d7bd
Vishesh Handa <me@vhanda.in>
2013-12-17 15:57:02 +0100

    Update Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,5 @@
 #!bin/sh
- 
+
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-#$PREPARETIPS > tips.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/nepomukwidgets.pot
-rm -f rc.cpp tips.cpp
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/baloowidgets.pot
+rm -f rc.cpp
