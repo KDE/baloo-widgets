fddb0fa03135ca2632c1a41658952ff16f9caf10
Lukáš Tinkl <lukas@kde.org>
2014-07-20 13:48:18 +0200

    fix Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #!bin/sh
 
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/baloowidgets.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/baloowidgets.pot
 rm -f rc.cpp
