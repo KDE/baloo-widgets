af254a77498364020f52098f655a759d30b525f6
Albert Astals Cid <aacid@kde.org>
2015-02-23 23:41:36 +0100

    Don't extract naturalqueryparser i18n twice
    CCMAIL: me@vhanda.in

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #!bin/sh
 
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/baloowidgets.pot
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg | grep -v "/src/naturalqueryparser/"` >> rc.cpp
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml | grep -v "/src/naturalqueryparser/"` -o $podir/baloowidgets.pot
 rm -f rc.cpp
