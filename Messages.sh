6a1ec9aaf1848ae219547bb147a58df3bbab20fa
Luigi Toscano <luigi.toscano@tiscali.it>
2017-04-20 23:17:50 +0200

    Rename the translation catalog (avoid conflicts)
    Avoid the conflict with the still-shipped kdelibs counterpart.

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #!bin/sh
 
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg | grep -v "/src/naturalqueryparser/"` >> rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml | grep -v "/src/naturalqueryparser/"` -o $podir/baloowidgets.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml | grep -v "/src/naturalqueryparser/"` -o $podir/baloowidgets5.pot
 rm -f rc.cpp
